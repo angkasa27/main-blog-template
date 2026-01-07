3943a9db780444206d200174aa9118bd8e30270a
Dimas Angkasa Nurindra <mas.angkasa27@gmail.com>
2026-01-07 16:06:13 +0700

    feat(prisma): clean up unused adapter code and ensure proper database URL usage

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+
+const accelerateUrl = process.env.DATABASE_URL!;
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
@@ -12,10 +14,10 @@ const globalForPrisma = global as unknown as {
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
+    // adapter,
+    accelerateUrl,
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
- 
